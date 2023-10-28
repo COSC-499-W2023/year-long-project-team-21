1aa210ed51e659667a53cf09fbdb311701c6b181
Sten <stenkorver@gmail.com>
2023-10-28 15:47:56 -0700

    register CustomerUser class

--- app/LooseTheLeftovers_Backend/Users/admin.py
+++ app/LooseTheLeftovers_Backend/Users/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = (
+        'username', 
+        'email', 
+        'password',
+        'last_name', 
+        'is_staff',
+        'is_active', 
+        'date_joined', 
+        'postal_code'
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+                )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+        ('Additional info', {
+            'fields': ('postal_code', )
+        })
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+                )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+        ('Additional info', {
+            'fields': ('postal_code')
+        })
+    )
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
