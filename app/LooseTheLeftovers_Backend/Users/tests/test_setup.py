48e6f658115b7789aa6d4133e2948280fff4f613
Sten <stenkorver@gmail.com>
2023-10-28 09:58:42 -0700

    change User to CustomerUser class

--- app/LooseTheLeftovers_Backend/Users/tests/test_setup.py
+++ app/LooseTheLeftovers_Backend/Users/tests/test_setup.py
@@ -1,20 +1,20 @@
 from rest_framework.test import APITestCase
-from django.contrib.auth.models import User
+from Users.models import CustomUser
 
 
 class TestSetUp(APITestCase):
     def setUp(self):
         # create temp user
-        self.test_user = User.objects.create_user(username="test", password="test123")
+        self.test_user = CustomUser.objects.create_user(username="test", password="test123", postal_code="V0E1V4")
         # save temp user credentials in db
         self.test_account = {
             "username": "test",
-            "password": "test132",
+            "password": "test123",
+            "postal_code": "V0E1V4",
         }
         # call APITestCase.setUp()
         return super().setUp()
 
     def tearDown(self):
-        self.test_user = User.objects.get(username="test")
-        self.test_user.delete()
+        self.test_user = CustomUser.objects.get(username="test")
         return super().tearDown()
