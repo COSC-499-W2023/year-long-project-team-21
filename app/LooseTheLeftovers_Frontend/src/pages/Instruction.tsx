dbaf2516eeab1d3d95fac7311b77ea38b6309480
n3c777 <n3c777@gmail.com>
2023-10-29 14:55:08 -0700

    changed instruction slightly so the text would fit

--- app/LooseTheLeftovers_Frontend/src/pages/Instruction.tsx
+++ app/LooseTheLeftovers_Frontend/src/pages/Instruction.tsx
@@ -13,24 +13,45 @@ function Instruction() {
         </Text>
         <Text style={[{top: 140}]}>
           <View>
-            <Title title=" The + button allows you to create an advertisment to share your food" />
+            <Title title=" The + button allows you to create" />
+          </View>
+        </Text>
+        <Text style={[{top: 140}]}>
+          <View>
+            <Title title=" an advertisment to share your food" />
+          </View>
+        </Text>
+        <Text style={[{top: 170}]}>
+          <View>
+            <Title title="You can see how long other people's posts" />
           </View>
         </Text>
         <Text style={[{top: 170}]}>
           <View>
             <Title
-              title="You can see other peoples posts aswell as how long they'll expire in
-          wether it's 2 days, or 2 weeks"
+              title="will expire in
+          wether it's 2 days or 2 weeks"
             />
           </View>
         </Text>
         <View>
           <Text style={[{top: 200}]}>
             <View>
-              <Title
-                title="You can view your account to see your current posts to edit or delete
-          them if you like"
-              />
+              <Title title="You can view your account to see your " />
+            </View>
+          </Text>
+        </View>
+        <View>
+          <Text style={[{top: 200}]}>
+            <View>
+              <Title title="current posts to edit or delete them " />
+            </View>
+          </Text>
+        </View>
+        <View>
+          <Text style={[{top: 200}]}>
+            <View>
+              <Title title="if you like" />
             </View>
           </Text>
         </View>
