115abef55325a3166475fc41d60fb34f8cb3b14b
n3c777 <n3c777@gmail.com>
2023-10-28 19:34:20 -0700

    instruction.tsx now uses components but it's not perfectly working

--- app/LooseTheLeftovers_Frontend/src/components/Title.tsx
+++ app/LooseTheLeftovers_Frontend/src/components/Title.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import { View, StyleSheet, Text, Image} from 'react-native'
-interface TitleProps{
-    title: string;
+import React from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+interface TitleProps {
+  title: string;
 }
 
 const Title: React.FC<TitleProps> = ({title}) => {
-    return (
-        <View style = {styles.TitleContainer}>
-            <Text style = {styles.TitleText}>{title}</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text style={styles.textTitle}>{title}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    TitleContainer:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-    },
-    TitleText:{
-        fontSize: 25,
-    }
-})
+  textTitle: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    paddingRight: 20,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
 
-export default Title
+export default Title;
--- app/LooseTheLeftovers_Frontend/src/pages/Instruction.tsx
+++ app/LooseTheLeftovers_Frontend/src/pages/Instruction.tsx
@@ -1,50 +1,39 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({
-  center: {
-    alignItems: 'center',
-    fontSize: 40,
-  },
-  container: {
-    position: 'absolute',
-    top: 300,
-    bottom: 0,
-    left: 300,
-    right: 0,
-    justifyContent: 'flex-end',
-  },
-  textTitle: {
-    textAlign: 'center',
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  belowTitle: {
-    textAlign: 'left',
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'black',
-    padding: 20,
-  },
-});
+import {Text, View} from 'react-native';
+import Title from '../components/Title';
 
 function Instruction() {
   return (
     <View>
-      <View style={[styles.center, {top: 70}]}>
-        <Text style={[styles.textTitle]}>Welcome to Lose the leftovers!</Text>
-        <Text style={[styles.belowTitle, {top: 80}]}>
-          The + button allows you to create an advertisment to share your food
+      <View>
+        <Text style={[{top: 90}]}>
+          <View>
+            <Title title="Welcome to Lose the leftovers!" />
+          </View>
         </Text>
-        <Text style={[styles.belowTitle, {top: 90}]}>
-          You can see other peoples posts aswell as how long they'll expire in
-          wether it's 2 days, or 2 weeks
+        <Text style={[{top: 140}]}>
+          <View>
+            <Title title=" The + button allows you to create an advertisment to share your food" />
+          </View>
         </Text>
-        <Text style={[styles.belowTitle, {top: 100}]}>
-          You can view your account to see your current posts to edit or delete
-          them if you like
+        <Text style={[{top: 170}]}>
+          <View>
+            <Title
+              title="You can see other peoples posts aswell as how long they'll expire in
+          wether it's 2 days, or 2 weeks"
+            />
+          </View>
         </Text>
+        <View>
+          <Text style={[{top: 200}]}>
+            <View>
+              <Title
+                title="You can view your account to see your current posts to edit or delete
+          them if you like"
+              />
+            </View>
+          </Text>
+        </View>
       </View>
     </View>
   );
