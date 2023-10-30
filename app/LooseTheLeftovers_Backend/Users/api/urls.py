b2700342479884e9f24116f8bb44d0daaf281449
CookiedOutMonster <gerren.14@gmail.com>
2023-10-30 10:50:56 -0700

    got rid of more hello world funkiness

--- app/LooseTheLeftovers_Backend/Users/api/urls.py
+++ app/LooseTheLeftovers_Backend/Users/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from Users.api.views import HelloWorld
 
 urlpatterns = [
     path("token", obtain_auth_token, name="token"),
