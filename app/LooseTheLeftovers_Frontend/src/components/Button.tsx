68361a3bcce4efbfdd873447f75e4bb8ad7e902e
Keizo Kato <keizo.kato410@gmail.com>
2023-11-06 15:06:16 -0800

    Button Compoenent is modified so that one can change textColor, borderColor, textSize, buttonSize(width) with proper commenting on Button.tsx

--- app/LooseTheLeftovers_Frontend/src/components/Button.tsx
+++ app/LooseTheLeftovers_Frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, SafeAreaView } from 'react-native';
 import styles from '../styles/buttonStyles';
+import React from 'react';
 
 /**
  * ButtonProps interface for the Button component.
@@ -11,6 +12,10 @@ import styles from '../styles/buttonStyles';
 interface ButtonProps {
   onPress: () => void;
   title: string;
+  textColor?: string;
+  borderColor?: string;
+  buttonSize?: number;
+  textSize?: number;
 }
 
 /**
@@ -20,18 +25,35 @@ interface ButtonProps {
  * @param {ButtonProps} props - The props for the Button component.
  * @param {() => void} props.onPress - Callback function to execute when the button is pressed.
  * @param {string} props.title - Text to display on the button.
- *
+ * @param {string} props.textColor - Text color in a Button component (defult:#555455)
+ * @param {string} props.borderColor - Border color for a Button component (defult: #ffb800)
+ * @param {number} props.buttonSize - change Button width (size)
+ * @param {number} props.textSize - change text font size in a Button. 
  * @example
  * <Button onPress={() => console.log('Button Pressed!')} title="Click Me" />
  */
-const Button: React.FC<ButtonProps> = ({ onPress, title }) => {
+const Button: React.FC<ButtonProps> = ({onPress, title, textColor, borderColor, textSize, buttonSize}) => {
+ 
+  //Set Default/Custom Button style
+  const buttonStyles = {
+    backgroundColor: 'white', // You can set a default background color or remove this line
+    borderColor: borderColor || '#ffb800', // Use the provided borderColor or a default value
+    width: buttonSize || 250, // Use the provided buttonSize or a default value
+  };
+
+  //Set Default/Custom Button style
+  const textStyles = {
+    color: textColor || '#555455', // Use the provided textColor or a default value
+    fontSize: textSize || 25, // Use the provided textSize or a default value
+  }
+
   return (
-    <>
+    <SafeAreaView>
       <View style={styles.space} />
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.buttonText}>{title}</Text>
+      <TouchableOpacity style={[styles.button,buttonStyles]} onPress={onPress}>
+        <Text style={[styles.buttonText, textStyles]}>{title}</Text>
       </TouchableOpacity>
-    </>
+    </SafeAreaView>
   );
 };
 
