3b7e3e8c86a6e0c559f97e934358c052d5355823
Egor <egormaxb@gmail.com>
2023-11-08 22:56:45 -0800

    Update to test remote image

--- app/LooseTheLeftovers_Frontend/__tests__/Icon.test.tsx
+++ app/LooseTheLeftovers_Frontend/__tests__/Icon.test.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react-native';
 import Icon from '../src/components/Icon';
+import Config from 'react-native-config';
 
-test('Icon component onPress event', () => {
-  const mockOnPress = jest.fn();
-  const testImageSource = require('../src/assets/test-home.png');
+describe('Icon Component Tests', () => {
+  it('triggers onPress event with local image', () => {
+    const mockOnPress = jest.fn();
+    const localImageSource = require('../src/assets/test-home.png');
 
-  const { getByTestId } = render(
-    <Icon 
-      source={testImageSource} 
-      size={30} 
-      onPress={mockOnPress}
-      testID="icon-test"
-    />
-  );
+    const { getByTestId } = render(
+      <Icon 
+        source={localImageSource} 
+        size={30} 
+        onPress={mockOnPress}
+        testID="icon-test"
+      />
+    );
 
-  const icon = getByTestId('icon-test');
-  fireEvent.press(icon);
+    const icon = getByTestId('icon-test');
+    fireEvent.press(icon);
 
-  expect(mockOnPress).toHaveBeenCalled();
-  expect(icon).toBeDefined();
+    expect(mockOnPress).toHaveBeenCalled();
+  });
+
+  it('triggers onPress event with remote image', () => {
+    const mockOnPress = jest.fn();
+    const remoteImageSource = { uri: Config.ICON_TEST_URL };
+
+    const { getByTestId } = render(
+      <Icon 
+        source={remoteImageSource} 
+        size={30} 
+        onPress={mockOnPress}
+        testID="icon-test-remote"
+      />
+    );
+
+    const icon = getByTestId('icon-test-remote');
+    fireEvent.press(icon);
+
+    expect(mockOnPress).toHaveBeenCalled();
+  });
 });
