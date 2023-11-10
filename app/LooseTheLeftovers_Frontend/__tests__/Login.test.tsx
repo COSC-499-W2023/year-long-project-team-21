eb615ed4ed454b4e225233b4da110401f3cd2db8
Egor <egormaxb@gmail.com>
2023-11-09 17:43:34 -0800

    Updated to test call to Storage + Strorage mock

--- app/LooseTheLeftovers_Frontend/__tests__/Login.test.tsx
+++ app/LooseTheLeftovers_Frontend/__tests__/Login.test.tsx
@@ -3,6 +3,7 @@ import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import axios from 'axios';
 import Login from '../src/screens/Login';
 import { Alert } from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 jest.mock('axios');
 jest.mock('react-native', () => {
@@ -10,20 +11,21 @@ jest.mock('react-native', () => {
   rn.Alert.alert = jest.fn();
   return rn;
 });
-  
+
 const navigation = {
   navigate: jest.fn(), // Mocking the navigate function
 };
 
 describe('Login component', () => {
-
   beforeEach(() => {
     jest.clearAllMocks(); // Clear all mocks including axios
   });
 
   it('renders correctly', () => {
-    const { getByPlaceholderText, getByText } = render(<Login naviagation={navigation} />);
-   
+    const { getByPlaceholderText, getByText } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Check if the username and password input fields are rendered
     expect(getByPlaceholderText('Username')).toBeTruthy();
     expect(getByPlaceholderText('Password')).toBeTruthy();
@@ -45,11 +47,12 @@ describe('Login component', () => {
     expect(getByPlaceholderText('Password').props.value).toBe('testpassword');
   });
 
-
   it('handles button press - success', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
-    const { getByPlaceholderText, getByText } = render(<Login navigation={navigation} />);
-    
+    const { getByPlaceholderText, getByText } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'testuser');
     // Simulate user input in the password field
@@ -61,28 +64,38 @@ describe('Login component', () => {
     await waitFor(() => {
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'testuser',
-          password: 'testpassword'
-        }
+          password: 'testpassword',
+        },
       );
+
       // Check if the expected success/failure message is displayed
       expect(Alert.alert).toHaveBeenCalledWith(
-        'Login Successful', 
-        'Token: fake_token'
+        'Login Successful',
+        'Token: fake_token',
       );
+
+      // Check if EncryptedStorage.setItem is called with the correct token
+      expect(EncryptedStorage.setItem).toHaveBeenCalledWith(
+        'user_token',
+        'fake_token',
+      );
+
       // assert any other expectations based on your API call
     });
   });
-   
-  it('handles button press - failure to send API request',async () => {
+
+  it('handles button press - failure to send API request', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
     // Mock a failed API response
     mockedAxios.post.mockRejectedValueOnce(new Error('API error'));
 
-    const { getByPlaceholderText, getByText } = render(<Login navigation = {navigation} />);
-    
+    const { getByPlaceholderText, getByText } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'testuser');
     // Simulate user input in the password field
@@ -93,27 +106,30 @@ describe('Login component', () => {
 
     // Wait for the asynchronous operation to complete
     await waitFor(() => {
-
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'testuser',
-          password: 'testpassword'
-        }
+          password: 'testpassword',
+        },
       );
       // Check if the expected success/failure message is displayed
       expect(Alert.alert).toHaveBeenCalledWith(
         'Error',
-        'An error occurred while trying to retrieve data.'
+        'An error occurred while trying to retrieve data.',
       );
+      // Check if EncryptedStorage.setItem is not called
+      expect(EncryptedStorage.setItem).not.toHaveBeenCalled();
     });
   });
 
-  it('handles button press - failure to login with invalid credentials',async () => {
+  it('handles button press - failure to login with invalid credentials', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
-    const { getByPlaceholderText, getByText } = render(<Login navigation={navigation} />);
-         
+    const { getByPlaceholderText, getByText } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'wrongtestuser');
     // Simulate user input in the password field
@@ -123,7 +139,7 @@ describe('Login component', () => {
     mockedAxios.post.mockResolvedValueOnce({
       status: 401,
       data: {
-         error: 'Invalid credentials',
+        error: 'Invalid credentials',
       },
     });
 
@@ -134,18 +150,19 @@ describe('Login component', () => {
     await waitFor(() => {
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'wrongtestuser',
-          password: 'wrongtestpassword'
-        }
+          password: 'wrongtestpassword',
+        },
       );
       // Check if the expected success/failure message is displayed
       expect(Alert.alert).toHaveBeenCalledWith(
         'Error',
-        'Failed to login or retrieve token.'
+        'Failed to login or retrieve token.',
       );
+      // Check if EncryptedStorage.setItem is not called
+      expect(EncryptedStorage.setItem).not.toHaveBeenCalled();
     });
   });
-  
 });
