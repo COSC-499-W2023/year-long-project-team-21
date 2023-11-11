9188da15265fdfb8ac2b07167dd6570748fc7913
Sten <stenkorver@gmail.com>
2023-11-10 21:27:58 -0800

    add temp url to blank template so update method in views.py can return

--- app/LooseTheLeftovers_Backend/Users/api/urls.py
+++ app/LooseTheLeftovers_Backend/Users/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from . import views
 
 
 urlpatterns = [
     path("token", obtain_auth_token, name="token"),
+    path("users/api/", views.edit_user_profile, name='users'),    
 ]
