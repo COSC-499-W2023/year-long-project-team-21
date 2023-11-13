35e587008be443ffc5d21ea9d751455324be6d61
CookiedOutMonster <gerren.14@gmail.com>
2023-11-13 12:15:30 -0800

    fixed line issues

--- app/LooseTheLeftovers_Backend/django.sh
+++ app/LooseTheLeftovers_Backend/django.sh
@@ -1,1 +1,12 @@
-test 
+#!/bin/bash
+echo "Create migrataions"
+python manage.py makemigrations
+echo "***************************************************"
+
+
+echo "Migrate"
+python manage.py migrate
+echo "***************************************************"
+
+echo "Start server on 0.0.0.0:8000"
+python manage.py runserver 0.0.0.0:8000
