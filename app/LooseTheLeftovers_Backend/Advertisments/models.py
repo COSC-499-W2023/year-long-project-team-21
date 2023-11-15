adc16a523c55896bc22a3ab53d0dcd218a2ccb07
Sten <stenkorver@gmail.com>
2023-11-14 16:28:33 -0800

    create initial model for advertisments

--- app/LooseTheLeftovers_Backend/Advertisments/models.py
+++ app/LooseTheLeftovers_Backend/Advertisments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Advertisment(models.Model):
+    user_id = models.IntegerField()
+    title = models.TextField(max_length=50)
+    description = models.TextField(max_length=1000)
+    #image = models.ImageField()
+    category = models.TextField(max_length=20)
+    expiry = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    view_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.000000)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.000000)
+
+    def __str__(self):
+        return str(self.pk) + ': ' + self.title
