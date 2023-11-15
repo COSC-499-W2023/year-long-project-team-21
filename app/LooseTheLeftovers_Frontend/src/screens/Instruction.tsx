41b3dac8db5699598482f55f5c60f1963071a8ef
n3c777 <n3c777@gmail.com>
2023-11-15 14:38:52 -0800

    added documentation for new instructions page

--- app/LooseTheLeftovers_Frontend/src/screens/Instruction.tsx
+++ app/LooseTheLeftovers_Frontend/src/screens/Instruction.tsx
@@ -8,39 +8,35 @@ import TextImage from '../components/TextImage';
 
 const Instruction = ({ navigation }: { navigation: any }) => {
   const handleButtonOnPress = async () => {};
+  //creates an array of the instructions and images
   const instructions = [
     {
-      key: '1',
       txt: 'See what others are offering',
       image: require('../assets/banana.png'),
       size: 60,
     },
     {
-      key: '2',
       txt: 'Select the range',
       image: require('../assets/select-range.png'),
       size: 120,
     },
     {
-      key: '3',
       txt: 'See how long food will expire in',
       image: require('../assets/expiry.png'),
       size: 80,
     },
     {
-      key: '3',
       txt: 'Create your own food ads',
       image: require('../assets/ad.png'),
       size: 60,
     },
     {
-      key: '4',
       txt: 'edit and delete ads with your account',
       image: require('../assets/account.png'),
       size: 60,
     },
   ];
-
+  //loops through the array and returns the entire instruction
   const list = () => {
     return instructions.map(element => {
       return (
@@ -63,9 +59,11 @@ const Instruction = ({ navigation }: { navigation: any }) => {
         />
       </View>
       <View style={style.instruction}>
-        <Texts //displays instruction
+        <Texts //displays the first text
           textsSize={20}
           texts="This is an app to share left over food to others in need"
+
+          //the list below displays the entire instruction and images
         />
         {list()}
       </View>
