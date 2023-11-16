508db1c8f09cfcee4c4bc085b812db75dd657c1c
n3c777 <n3c777@gmail.com>
2023-11-16 02:27:38 -0800

    added testing and documentation for instructions component TextImage

--- app/LooseTheLeftovers_Frontend/src/screens/Instruction.tsx
+++ app/LooseTheLeftovers_Frontend/src/screens/Instruction.tsx
@@ -11,26 +11,31 @@ const Instruction = ({ navigation }: { navigation: any }) => {
   //creates an array of the instructions and images
   const instructions = [
     {
+      id: 1,
       txt: 'See what others are offering',
       image: require('../assets/banana.png'),
       size: 60,
     },
     {
+      id: 2,
       txt: 'Select the range',
       image: require('../assets/select-range.png'),
       size: 120,
     },
     {
+      id: 3,
       txt: 'See how long food will expire in',
       image: require('../assets/expiry.png'),
       size: 80,
     },
     {
+      id: 4,
       txt: 'Create your own food ads',
       image: require('../assets/ad.png'),
       size: 60,
     },
     {
+      id: 5,
       txt: 'edit and delete ads with your account',
       image: require('../assets/account.png'),
       size: 60,
@@ -41,6 +46,7 @@ const Instruction = ({ navigation }: { navigation: any }) => {
     return instructions.map(element => {
       return (
         <TextImage
+          key={element.id}
           text={element.txt}
           source={element.image}
           size={element.size}
