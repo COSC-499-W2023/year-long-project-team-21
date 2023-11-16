bae7fb163d9479e1614a6dac47e6667866ac4996
Egor <egormaxb@gmail.com>
2023-11-16 12:18:30 -0500

    Updated to check for error messages

--- app/LooseTheLeftovers_Frontend/__tests__/Login.test.tsx
+++ app/LooseTheLeftovers_Frontend/__tests__/Login.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import axios from 'axios';
-import { Alert } from 'react-native';
 import Login from '../src/screens/Login';
 
 jest.mock('axios');
@@ -10,20 +9,21 @@ jest.mock('react-native', () => {
   rn.Alert.alert = jest.fn();
   return rn;
 });
-  
+
 const navigation = {
   navigate: jest.fn(), // Mocking the navigate function
 };
 
 describe('Login component', () => {
-
   beforeEach(() => {
     jest.clearAllMocks(); // Clear all mocks including axios
   });
 
   it('renders correctly', () => {
-    const { getByPlaceholderText, getByTestId } = render(<Login navigation={navigation} />);
-   
+    const { getByPlaceholderText, getByTestId } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Check if the username and password input fields are rendered
     expect(getByPlaceholderText('Username')).toBeTruthy();
     expect(getByPlaceholderText('Password')).toBeTruthy();
@@ -49,11 +49,12 @@ describe('Login component', () => {
     expect(getByPlaceholderText('Password').props.value).toBe('testpassword');
   });
 
-
   it('handles button press - success', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
-    const { getByPlaceholderText, getByTestId } = render(<Login navigation={navigation} />);
-    
+    const { getByPlaceholderText, getByTestId, queryByTestId } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'testuser');
     // Simulate user input in the password field
@@ -65,28 +66,49 @@ describe('Login component', () => {
     await waitFor(() => {
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'testuser',
-          password: 'testpassword'
-        }
+          password: 'testpassword',
+        },
       );
-      // Check if the expected success/failure message is displayed
-      expect(Alert.alert).toHaveBeenCalledWith(
-        'Login Successful', 
-        'Token: fake_token'
-      );
-      // assert any other expectations based on your API call
+      // Since this is a success scenario, check that the error message is not displayed
+      expect(queryByTestId('error-msg')).toBeNull();
     });
   });
-   
-  it('handles button press - failure to send API request',async () => {
+
+  it('navigates to the next screen on successful login', async () => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>;
+    // Mock a successful API response
+    mockedAxios.post.mockResolvedValueOnce({
+      status: 200,
+      data: { token: 'fake_token' },
+    });
+
+    const { getByPlaceholderText, getByTestId } = render(
+      <Login navigation={navigation} />,
+    );
+
+    // Simulate user input and button press
+    fireEvent.changeText(getByPlaceholderText('Username'), 'testuser');
+    fireEvent.changeText(getByPlaceholderText('Password'), 'testpassword');
+    fireEvent.press(getByTestId('loginButton'));
+
+    await waitFor(() => {
+      // Check if navigation was triggered with the correct screen name
+      expect(navigation.navigate).toHaveBeenCalledWith('Instruction');
+    });
+  });
+
+  it('handles button press - failure to send API request', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
     // Mock a failed API response
     mockedAxios.post.mockRejectedValueOnce(new Error('API error'));
 
-    const { getByPlaceholderText, getByTestId } = render(<Login navigation = {navigation} />);
-    
+    const { getByPlaceholderText, getByTestId, queryByTestId } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'testuser');
     // Simulate user input in the password field
@@ -97,27 +119,30 @@ describe('Login component', () => {
 
     // Wait for the asynchronous operation to complete
     await waitFor(() => {
-
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'testuser',
-          password: 'testpassword'
-        }
+          password: 'testpassword',
+        },
       );
-      // Check if the expected success/failure message is displayed
-      expect(Alert.alert).toHaveBeenCalledWith(
-        'Error',
-        'An error occurred while trying to retrieve data.'
+      // Find the error message element
+      const errorMessageElement = getByTestId('error-msg');
+
+      // Check if the error message text is correct
+      expect(errorMessageElement.props.children).toBe(
+        'An error occurred while trying to retrieve data.',
       );
     });
   });
 
-  it('handles button press - failure to login with invalid credentials',async () => {
+  it('handles button press - failure to login with invalid credentials', async () => {
     const mockedAxios = axios as jest.Mocked<typeof axios>;
-    const { getByPlaceholderText, getByTestId } = render(<Login navigation={navigation} />);
-         
+    const { getByPlaceholderText, getByTestId, queryByTestId } = render(
+      <Login navigation={navigation} />,
+    );
+
     // Simulate user input in the username field
     fireEvent.changeText(getByPlaceholderText('Username'), 'wrongtestuser');
     // Simulate user input in the password field
@@ -127,7 +152,7 @@ describe('Login component', () => {
     mockedAxios.post.mockResolvedValueOnce({
       status: 401,
       data: {
-         error: 'Invalid credentials',
+        error: 'Invalid credentials',
       },
     });
 
@@ -138,18 +163,19 @@ describe('Login component', () => {
     await waitFor(() => {
       // Check if the Axios POST request is called with the correct arguments
       expect(mockedAxios.post).toHaveBeenCalledWith(
-        "http://10.0.2.2:8000/users/token",
+        'http://10.0.2.2:8000/users/token',
         {
           username: 'wrongtestuser',
-          password: 'wrongtestpassword'
-        }
+          password: 'wrongtestpassword',
+        },
       );
+      // Find the error message element
+      const errorMessageElement = getByTestId('error-msg');
+
       // Check if the expected success/failure message is displayed
-      expect(Alert.alert).toHaveBeenCalledWith(
-        'Error',
-        'Failed to login or retrieve token.'
+      expect(errorMessageElement.props.children).toBe(
+        'Failed to login or retrieve token.',
       );
     });
   });
-  
 });
