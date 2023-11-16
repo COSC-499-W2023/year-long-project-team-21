ce04ab8abba927432a5cbac83cd87b5675b8f6ae
Egor <egormaxb@gmail.com>
2023-11-16 12:17:57 -0500

    Added testID prop

--- app/LooseTheLeftovers_Frontend/src/components/Text.tsx
+++ app/LooseTheLeftovers_Frontend/src/components/Text.tsx
@@ -11,6 +11,7 @@ import styles from '../styles/textStyles';
  * @property {string} textsColor - Changes Text color.
  * @property {string} position - Changes Text position on the screen.
  * @property {() => void} onPress - Callback function executed when the button is pressed.
+ * @property {string} testID - Used to identify an element in testing.
  */
 interface TextsProps {
   texts: string;
@@ -18,6 +19,7 @@ interface TextsProps {
   textsColor?: string;
   position?: string;
   onPress?: () => void;
+  testID?: string;
 }
 
 /**
@@ -31,6 +33,7 @@ interface TextsProps {
  * @param {string} props.textsColor - Color specification.
  * @param {string} props.position - Determines Text position.
  * @param {() => void} props.onPress - Callback function to execute when the button is pressed.
+ * @param {string} props.testID - Identify Text in testing.
  * @example
  * <Texts texts="Words" />
  */
@@ -41,6 +44,7 @@ const Texts: React.FC<TextsProps> = ({
   textsColor,
   position,
   onPress,
+  testID,
 }) => {
   // Default/Custom styles for Text component
   // If any custom values are passed as props, apply them, default otherwise
@@ -53,7 +57,7 @@ const Texts: React.FC<TextsProps> = ({
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={styles.TitleContainer}>
-        <Text style={[styles.TitleText, textsStyles]}>{texts}</Text>
+        <Text style={[styles.TitleText, textsStyles]} testID={testID}>{texts}</Text>
       </View>
     </TouchableOpacity>
   );
