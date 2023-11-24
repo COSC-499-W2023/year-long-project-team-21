4171ab3b047b77cf5b6d59ff95a96418e220fd81
Sten <stenkorver@gmail.com>
2023-11-23 20:50:56 -0800

    update views url

--- app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
+++ app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
-# import views
 from . import views
-from Advertisments.api.views import add_advertisment
 
 urlpatterns = [
-    path("", add_advertisment, name="create_ad"),
+    path("", views.AdvertismentView.as_view(), name="create_ad"),
 ]
