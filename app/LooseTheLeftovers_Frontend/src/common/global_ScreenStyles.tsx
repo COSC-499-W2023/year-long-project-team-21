f7887ec8bbf36f3f7bff402e52e5906300ce376d
n3c777 <n3c777@gmail.com>
2023-11-26 16:38:31 -0800

    added global screen styles

---
+++ app/LooseTheLeftovers_Frontend/src/common/global_ScreenStyles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const globalscreenstyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+});
+export default globalscreenstyles;
