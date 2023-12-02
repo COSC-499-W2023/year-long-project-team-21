a08bbcbfb4c44d93808a9cc38ffc1f55bd3d8472
Sten <stenkorver@gmail.com>
2023-12-02 09:16:36 -0800

    remove not needed files

--- app/LooseTheLeftovers_Backend/Advertisments/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Advertisment
-
-class AdvertismentForm(ModelForm):
-    class Meta:
-        model = Advertisment
-        fields = ['user_id', 'title','description','category','expiry']
