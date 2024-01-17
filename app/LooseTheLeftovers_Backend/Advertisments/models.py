a795dac1b597101d41023d0699bd1a3afcc46112
Sten <stenkorver@gmail.com>
2024-01-16 17:45:19 -0800

    add AdvertismentImage model

--- app/LooseTheLeftovers_Backend/Advertisments/models.py
+++ app/LooseTheLeftovers_Backend/Advertisments/models.py
@@ -6,7 +6,6 @@ class Advertisment(models.Model):
     user_id = models.IntegerField()
     title = models.TextField(max_length=50)
     description = models.TextField(max_length=1000)
-    #image = models.ImageField()
     category = models.TextField(max_length=20)
     expiry = models.DateTimeField(null=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -14,6 +13,13 @@ class Advertisment(models.Model):
     is_active = models.BooleanField(default=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.000000)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0.000000)
-
+    
     def __str__(self):
         return str(self.pk) + ': ' + self.title
+
+class AdvertismentImage(models.Model):
+    ad_id = models.OneToOneField(Advertisment, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return str(self.pk) + ': ' + self.image.url + ' linked to ad number: ' + str(self.ad_id)
