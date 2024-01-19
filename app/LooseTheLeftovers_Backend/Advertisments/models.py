56df56aaa878b46b44d0918acb1adb2075f554e7
Sten <stenkorver@gmail.com>
2024-01-18 17:40:28 -0800

    make description optional and remove unused import

--- app/LooseTheLeftovers_Backend/Advertisments/models.py
+++ app/LooseTheLeftovers_Backend/Advertisments/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-import datetime
 
 # Create your models here.
 class Advertisment(models.Model):
     user_id = models.IntegerField()
     title = models.TextField(max_length=50)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000, null=True)
     category = models.TextField(max_length=20)
     expiry = models.DateTimeField(null=True)
     created = models.DateTimeField(auto_now_add=True)
