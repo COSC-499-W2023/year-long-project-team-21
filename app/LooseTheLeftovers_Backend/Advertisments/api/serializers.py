5545f11824ac33e0a50a3d32938314c7ff9e6bc2
Sten <stenkorver@gmail.com>
2024-01-18 17:41:24 -0800

    improve documentation for classes and methods

--- app/LooseTheLeftovers_Backend/Advertisments/api/serializers.py
+++ app/LooseTheLeftovers_Backend/Advertisments/api/serializers.py
@@ -4,16 +4,27 @@ from Advertisments.models import Advertisment, AdvertismentImage
 class AdvertismentSerializer(serializers.Serializer):
     '''
     Serializer to validate data fields for the Advertisment model.
+
+    When an instance of this class is initialized it has to be passed the request data
+    as a parameter. After it is initialized .is_valid() should be called to validate the data.
+
+    If .is_valid() returns false, the .errors property will be populated showing which fields
+    could not be validated.
+
+    If .is_valid() returns true, the .validated_data property will be populated and the .save() 
+    method can be called to save an instance of an Advertisment in the database.
     '''
     title = serializers.CharField(max_length=50)
-    description = serializers.CharField(max_length=1000)
+    description = serializers.CharField(max_length=1000, required=False)
     category = serializers.CharField(max_length=30)
     expiry = serializers.DateTimeField(required=False)
     
     def create(self, validated_data):
         '''
-        Create method executes when AdvertismentSerializer.save() is called. Will save an instance
-        of the Advertisment in the database
+        Create method executes when AdvertismentSerializer.save() is called in advertisements/api/views.py. 
+        Will save an instance of the Advertisment in the database.
+
+        Returns an instance of the Advertisment that was saved.
         '''
         user = self.context['request'].user
         ad = Advertisment.objects.create(
@@ -25,13 +36,24 @@ class AdvertismentSerializer(serializers.Serializer):
 class ImageSerializer(serializers.Serializer):
     '''
     Serializer to validate data fields for the AdvertismentImage model.
+
+    When an instance of this class is initialized it has to be passed the request files
+    as a parameter. After it is initialized .is_valid() should be called to validate that
+    the image passed is valid.
+    
+    If .is_valid() returns false, the .errors property will be populated.
+
+    If .is_valid() returns true, the .validated_data property will be populated and the .save() 
+    method can be called to save an instance of an AdvertismentImage in the database.
     '''
     image = serializers.ImageField()
     
     def create(self, validated_data):
         '''
         Create method executes when ImageSerializer.save() is called. Will save an instance
-        of the AdvertismentImage in the database
+        of the AdvertismentImage in the database.
+
+        Returns an instance of the AdvertismentImage that was saved.
         '''
         ad = self.context['ad']
         image = AdvertismentImage.objects.create(
