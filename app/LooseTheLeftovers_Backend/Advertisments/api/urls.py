d28d380bf075a0a8d88053bb01c1475739d8b49b
Sten <stenkorver@gmail.com>
2024-01-23 21:45:49 -0800

    add endpoints to retrieve ads

--- app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
+++ app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.create_advertisment, name="create-ad"),
+    path("<int:ad_id>/", views.get_advertisment, name="specific-ad"),
+    path("<int:user_id>/", views.get_advertisment, name="user-ads"),
+    path("", views.get_advertisment, name="ads"),
 ]
