8a5e681e4dce8a3cb944829eb46d00f675d7202c
Sten <stenkorver@gmail.com>
2024-01-24 18:54:03 -0800

    add tests for GET requests retrieving ads

--- app/LooseTheLeftovers_Backend/Advertisments/tests/test_views.py
+++ app/LooseTheLeftovers_Backend/Advertisments/tests/test_views.py
@@ -1,15 +1,14 @@
 from Advertisments.models import Advertisment, AdvertismentImage
 from django.db.models.fields.files import ImageFieldFile
-from .test_setup import TestSetUpCreateAdvertisment
+from .test_setup import TestSetUpCreateAdvertisment, TestSetUpRetrieveAdvertisment
 from django.urls import reverse
 from rest_framework import status
 
 from rest_framework.test import APIClient
 
-class TestModels(TestSetUpCreateAdvertisment):
+class TestCreateAd(TestSetUpCreateAdvertisment):
 
-    __create_ad_url = reverse("create-ad")
-    __get_ad_url = reverse("ads")
+    __ad_url = reverse("create-ad")
 
     def test_post_new_ad(self):
         """
@@ -19,7 +18,7 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request and assert valid response
         response = client.post(
-            self.__create_ad_url,
+            self.__ad_url,
             self.valid_data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
@@ -41,7 +40,7 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request with invalid token
         response = client.post(
-            self.__create_ad_url,
+            self.__ad_url,
             self.valid_data,
             HTTP_AUTHORIZATION='Bearer ' + 'this_is_not_a_valid_token',
             format="multipart",
@@ -64,7 +63,7 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request and assert valid response
         response = client.post(
-            self.__create_ad_url,
+            self.__ad_url,
             data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
@@ -86,7 +85,7 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request and assert valid response
         response = client.post(
-            self.__create_ad_url,
+            self.__ad_url,
             data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
@@ -107,7 +106,7 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request and assert valid response
         response = client.post(
-            self.__create_ad_url,
+            self.__ad_url,
             data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
@@ -129,7 +128,7 @@ class TestModels(TestSetUpCreateAdvertisment):
         
         # post request and assert valid response
         response = client.get(
-            self.__create_ad_url,
+            self.__ad_url,
             self.valid_data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
@@ -144,13 +143,15 @@ class TestModels(TestSetUpCreateAdvertisment):
 
         # post request and assert valid response
         response = client.put(
-            self.__create_ad_url,
+            self.__ad_url,
             self.valid_data,
             HTTP_AUTHORIZATION='Bearer ' + self.token,
             format="multipart",
         )
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
+
+class TestRetrieveAds(TestSetUpRetrieveAdvertisment):
+
     def test_get_single_ad(self):
         """
         Test if single ad can be retrieved via GET request. Expect HTTP_200_OK response and the ad
@@ -159,12 +160,74 @@ class TestModels(TestSetUpCreateAdvertisment):
         # TODO: also needs to assert the ad image was returned
         client = APIClient()
 
-        # get request and assert valid response
-        data = {'ad_id': 1}
-        response = client.get(
-            self.__get_ad_url,
-            data,
-            HTTP_AUTHORIZATION='Bearer ' + self.token,
-            format="json",
+        # specific user id to get all ads for
+        specific_ad_id = 10
+
+        # create get request using kwargs
+        specific_ad_url = reverse(
+            "specific-ad", 
+            kwargs={"ad_id": specific_ad_id}
         )
+
+        # send request
+        response = client.get(specific_ad_url)
+
+        # assert valid response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # assert ad matching primary key is returned
+        self.assertEqual(response.data[0]['id'], 10)
+
+        # assert path to image is included
+        self.assertEqual(response.data[1]['image'], '/media/app/LooseTheLeftovers_Backend/media/images/12345.PNG')
+
+    def test_get_users_ads(self):
+        """
+        Test if single ad can be retrieved via GET request. Expect HTTP_200_OK response and the ad
+        information in returned as json
+        """
+        # TODO: also needs to assert the ad image was returned
+        client = APIClient()
+
+        # specific user id to get all ads for
+        specific_user_id = 1
+
+        # create get request using kwargs
+        user_ad_url = reverse(
+            "user-ads", 
+            kwargs={"user_id": specific_user_id}
+        )
+
+        # send request
+        response = client.get(user_ad_url)
+
+        # assert valid response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # assert 3 ads returned 
+        self.assertEqual(len(response.data[0]), 3)
+
+        # assert 3 images returned
+        self.assertEqual(len(response.data[1]), 3)
+
+    def test_get_all_ads(self):
+        """
+        Test if single ad can be retrieved via GET request. Expect HTTP_200_OK response and the ad
+        information in returned as json
+        """
+        # TODO: also needs to assert the ad image was returned
+        client = APIClient()
+
+        all_ad_url = reverse("all-ads")
+
+        # send request
+        response = client.get(all_ad_url)
+
+        # assert valid response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # assert 5 ads returned 
+        self.assertEqual(len(response.data[0]), 5)
+
+        # assert 5 images returned
+        self.assertEqual(len(response.data[1]), 5)
