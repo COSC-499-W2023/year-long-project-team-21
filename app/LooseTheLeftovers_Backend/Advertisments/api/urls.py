7cffcb7cca5f8b5a42a28bf98ff8aa18c353497f
Sten <stenkorver@gmail.com>
2024-01-25 19:13:56 -0800

    standardize urls

--- app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
+++ app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
@@ -4,6 +4,6 @@ from Advertisments.api.views import AdvertismentHandler
 urlpatterns = [
     path("", AdvertismentHandler.as_view(), name="all-ads"),
     path("<int:ad_id>/", AdvertismentHandler.as_view(), name="specific-ad"),
-    path("user/<int:user_id>/", AdvertismentHandler.as_view(), name="user-ads"),
-    path("create", AdvertismentHandler.as_view(), name="create-ad")
+    path("users/<int:user_id>/", AdvertismentHandler.as_view(), name="user-ads"),
+    path("", AdvertismentHandler.as_view(), name="create-ad")
 ]
