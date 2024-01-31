cc184dd766843b0a7649119f02020ab5146b6fa9
n3c777 <n3c777@gmail.com>
2024-01-30 19:37:30 -0800

    added gradient for home screen

--- app/LooseTheLeftovers_Frontend/src/screens/Home.tsx
+++ app/LooseTheLeftovers_Frontend/src/screens/Home.tsx
@@ -3,7 +3,7 @@ import { ScrollView, View } from 'react-native';
 import TabBar from '../components/TabBar';
 
 import globalscreenstyles from '../common/global_ScreenStyles';
-
+import { global } from '../common/global_styles';
 import Logo from '../components/Logo';
 import CreateAdIcon from '../components/CreateAdIcon';
 import MessageIcon from '../components/MessageIcon';
@@ -12,6 +12,8 @@ import AccountIcon from '../components/AccountIcon';
 import PostListRenderer from '../components/PostListRenderer';
 import LocationService from '../common/LocationService';
 import View_Post from './View_Post';
+import LinearGradient from 'react-native-linear-gradient';
+
 const Home = ({ navigation }: { navigation: any }) => {
   // const [hasLocationPermission, setHasLocationPermission] = useState<boolean | null>(null);
   // const checkLocationPermission = async () => {
@@ -23,22 +25,29 @@ const Home = ({ navigation }: { navigation: any }) => {
   //     setHasLocationPermission(false); // Assume no permission in case of an error
   //   }
   // };
-  
+
   return (
-    <View style={globalscreenstyles.container}>
+    <LinearGradient
+      style={globalscreenstyles.container}
+      colors={[
+        global.background,
+        global.post_color.expiry_mid[0],
+        global.background,
+      ]}
+      start={{ x: 0, y: 0 }}>
       <TabBar
         LeftIcon={<Logo LogoSize={15}></Logo>}
         RightIcon={<MessageIcon></MessageIcon>}></TabBar>
 
       <View style={globalscreenstyles.body}>
-        <PostListRenderer isHeaderInNeed={true}  navigation={navigation}/>
+        <PostListRenderer isHeaderInNeed={true} navigation={navigation} />
       </View>
 
       <TabBar
         LeftIcon={<HomeIcon></HomeIcon>}
         MiddleIcon={<CreateAdIcon></CreateAdIcon>}
         RightIcon={<AccountIcon></AccountIcon>}></TabBar>
-    </View>
+    </LinearGradient>
   );
 };
 
