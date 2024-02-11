88c305e9d15aad94fddc04b8f5f12fdb87a64a60
n3c777 <n3c777@gmail.com>
2024-02-11 01:49:47 -0800

    more colour change

--- app/LooseTheLeftovers_Frontend/src/common/global_styles.tsx
+++ app/LooseTheLeftovers_Frontend/src/common/global_styles.tsx
@@ -9,7 +9,7 @@ export const global = {
 
   //darkmode
   // primary: '#2CAF55',
-  primary: '#2CBD51',
+  primary: '#2FC956',
   purple: '#251D3A',
   //purple: '#2E0C22',
   secondary: '#FFFFFF', //white
@@ -40,15 +40,10 @@ export const global = {
 
   //post color [original, lighter, even lighter]
   post_color: {
-    // expiry_short: ['#6B142D', '#BB3D60', '#E97D8A'],
-    //expiry_mid: ['#251D3A', '#624E95', '#DFBDE1'],
-    //expiry_long: ['#0F5A44', '#1D8352', '#73E485'],
+    expiry_short: ['#2e2e2e', '#E0495A', '#E07581'],
 
-    expiry_short: ['#2e2e2e', '#E05A73', '#E996A9'],
-    //expiry_short: ['#2e2e2e', '#F96F58', '#FF9685'],
-    expiry_mid: ['#2e2e2e', '#FFB027', '#FFC8AA'], //yellow
-    //expiry_long: ['#2e2e2e', '#BB3D60', '#E97D8A'],
-    // expiry_mid: ['#2e2e2e', '#624E95', '#DFBDE1'],
-    expiry_long: ['#2e2e2e', '#1D8352', '#A6E4AF'],
+    expiry_mid: ['#2e2e2e', '#FFB72E', '#FFC8AA'], //yellow
+
+    expiry_long: ['#2e2e2e', '#2FBD35', '#A6E4AF'],
   },
 };
