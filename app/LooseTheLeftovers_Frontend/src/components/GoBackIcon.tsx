12a3638b14df861ff24e3c485b36e41a71464a6f
n3c777 <n3c777@gmail.com>
2024-02-11 14:23:05 -0800

    ctreated tests changed sizing and position

--- app/LooseTheLeftovers_Frontend/src/components/GoBackIcon.tsx
+++ app/LooseTheLeftovers_Frontend/src/components/GoBackIcon.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { type AllIconProps } from '../common/Types';
 
 /**
  * Icon component.
  *
- * A clickable homeicon component that be used in various parts of the application including tab.
+ * A clickable goback component that be used in various parts of the application including tab.
  * Will take you to the create ad screen
  *
  * @component
  * @param {IconProps} props - The props for the Icon component.
+ *
  * @param {ImageSourcePropType} props.source - The image source for the icon.
  * @param {number} [props.size=45] - The size of the icon.
  * @param {() => void} props.onPress - Callback function to execute when the icon is pressed.
@@ -21,30 +22,36 @@ import { type AllIconProps } from '../common/Types';
 
 //creates the parameters we're sending
 type RootStackParamList = {
-  CreateAd: { name: string };
+  GoBack: { name: string };
 };
 
 //uses all icon prop for the interface
-const GoBackIcon: React.FC<AllIconProps> = ({ size = 48 }) => {
+const GoBackIcon: React.FC<AllIconProps> = ({ size = 30 }) => {
   //creates a navigation hook
   const navigation =
-    useNavigation<NativeStackNavigationProp<RootStackParamList, 'CreateAd'>>();
+    useNavigation<NativeStackNavigationProp<RootStackParamList, 'GoBack'>>();
 
   //link to image
   const image = '../assets/back_arrow_white.png';
 
   //navigates to instruction and sends parameters
-  const goToHomeScreen = () => {
+  const goBack = () => {
     navigation.goBack();
   };
 
-  //call this to display and icon image. No need to input anything to the component, will automatically
-  //set the image size and navigation. Yhis is Good for reusability.
+  const goBackStyles = StyleSheet.create({
+    container: {
+      paddingTop: '45%',
+      marginRight: '30%',
+    },
+  });
+  // call this to display and icon image. No need to input anything to the component, will automatically
+  // set the image size and navigation. This is Good for reusability.
   return (
-    <TouchableOpacity onPress={goToHomeScreen} testID={'CreateAdIconTest'}>
+    <TouchableOpacity onPress={goBack} testID={'GoBackIconTest'}>
       <Image
         source={require(image)}
-        style={[{ width: size, height: size }]}
+        style={[{ width: size, height: size }, goBackStyles.container]}
         resizeMode="contain"
       />
     </TouchableOpacity>
