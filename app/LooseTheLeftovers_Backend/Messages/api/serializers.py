0e784ce9744bbb5baaa9c739ac5c87448c6b348a
Sten <stenkorver@gmail.com>
2024-02-11 18:47:04 -0800

    add serialiazers for creating and retrieving messages

--- app/LooseTheLeftovers_Backend/Messages/api/serializers.py
+++ app/LooseTheLeftovers_Backend/Messages/api/serializers.py
@@ -1,1 +1,39 @@
 from rest_framework import serializers
+
+from Messages.models import Message
+
+class MessageSerializer(serializers.Serializer):
+    """
+    Serializer to validate data fields for the Message model.
+
+    When an instance of this class is initialized it has to be passed the request data
+    as a parameter. After it is initialized .is_valid() should be called to validate the data.
+
+    If .is_valid() returns false, the .errors property will be populated showing which fields
+    could not be validated.
+
+    If .is_valid() returns true, the .validated_data property will be populated and the .save()
+    method can be called to save an instance of an Message in the database.
+    """
+
+    msg = serializers.CharField(max_length=1000)
+    receiver_id = serializers.IntegerField()
+    ad_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        """
+        Create method executes when AdvertismentSerializer.save() is called in advertisements/api/views.py.
+        Will save an instance of the Advertisment in the database.
+
+        Returns an instance of the Advertisment that was saved.
+        """
+        user = self.context["request"].user
+        ad = Message.objects.create(user_id=user.id, **validated_data)
+        return ad
+
+class GetMessageSerializer(serializers.Serializer):
+    msg = serializers.CharField(max_length=1000)
+    sender_id = serializers.IntegerField()
+    receiver_id = serializers.IntegerField()
+    ad_id = serializers.IntegerField()
+    time_sent = serializers.DateTimeField()
