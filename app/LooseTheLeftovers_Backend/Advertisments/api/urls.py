f63a0ec601efbb8fd997e91e19c900cfe94e4818
Sten <stenkorver@gmail.com>
2024-03-05 19:57:19 -0800

    add endpoint to get ads by category

--- app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
+++ app/LooseTheLeftovers_Backend/Advertisments/api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", AdvertismentHandler.as_view(), name="all-ads"),
     path("<int:ad_id>/", AdvertismentHandler.as_view(), name="specific-ad"),
     path("users/<int:user_id>/", AdvertismentHandler.as_view(), name="user-ads"),
-    path("", AdvertismentHandler.as_view(), name="create-ad")
+    path("", AdvertismentHandler.as_view(), name="create-ad"),
+    path("location/", AdvertismentHandler.as_view(), name="location-ads"),
+    path("category/", AdvertismentHandler.as_view(), name="category-ads"),
 ]
