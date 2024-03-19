8f767d9b6a37f3708963ebf2c7d5c6c367376e81
n3c777 <n3c777@gmail.com>
2024-03-19 12:04:16 -0700

    logo is now clickable, and instructions has been modified

--- app/LooseTheLeftovers_Frontend/src/components/Logo.tsx
+++ app/LooseTheLeftovers_Frontend/src/components/Logo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { TouchableOpacity, Image, View } from 'react-native';
+
 import { type AllIconProps } from '../common/Types';
 
 /**
@@ -13,30 +14,41 @@ import { type AllIconProps } from '../common/Types';
  * @component
  * @param {IconProps} props - The props for the Icon component.
  * @param {ImageSourcePropType} props.source - The image source for the icon.
+ * @param {() => void} props.onPress - Callback function to execute when the icon is pressed.
  * @param {number} [props.size=45] - The size of the icon.
  * @param {string} [props.testID] - Optional test identifier for the component.
  * @example
  */
 
 //uses all icon prop for the interface
-const Logo: React.FC<AllIconProps> = ({ size = 63 }) => {
+const Logo: React.FC<AllIconProps> = ({ onPress, size = 63 }) => {
   //creates a navigation hook
 
+  type RootStackParamList = {
+    Instruction: { name: string };
+  };
   //link to image
   const image = '../assets/logo.png';
 
-  //navigates to Home and sends parameters
+  const navigation =
+    useNavigation<
+      NativeStackNavigationProp<RootStackParamList, 'Instruction'>
+    >();
+
+  const goToInstructionScreen = () => {
+    navigation.navigate('Instruction', { name: 'Logo' });
+  };
 
   //call this to display and icon image. No need to input anything to the component, will automatically
-  //set the image size and navigation. Yhis is Good for reusability.
+  //set the image size and navigation. This is Good for reusability.
   return (
-    <View testID={'HomeIconTest'}>
+    <TouchableOpacity onPress={goToInstructionScreen} testID={'HomeIconTest'}>
       <Image
         source={require(image)}
         style={[{ width: size, height: size }]}
         resizeMode="contain"
       />
-    </View>
+    </TouchableOpacity>
   );
 };
 
