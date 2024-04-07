2008bfa43dbbfaad3d25bf21803dac97decb1ec4
n3c777 <n3c777@gmail.com>
2024-04-07 10:08:27 -0700

    added screen transition changes that were previously removed for some reason

--- app/LooseTheLeftovers_Frontend/App.tsx
+++ app/LooseTheLeftovers_Frontend/App.tsx
@@ -3,11 +3,18 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { navigationRef } from './src/navigation/NavigationService';
 import { GlobalProvider } from './src/common/GlobalContext';
-
+const LoadingScreenColor = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#2e2e2e',
+  },
+};
 const App = () => {
   return (
     <GlobalProvider>
       <NavigationContainer
+        theme={LoadingScreenColor}
         ref={navigationRef}
         data-testid="navigation-container">
         <AppNavigator />
